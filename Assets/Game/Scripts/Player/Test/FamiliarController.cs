d4c4d09a0eefd61e8310d70fe1e79a90776b15b2
OmerAsik <faruk.asikoglu@gmail.com>
2022-01-29 02:59:03 +0300

    Update: Familiars

--- Assets/Game/Scripts/Player/Test/FamiliarController.cs
+++ Assets/Game/Scripts/Player/Test/FamiliarController.cs
@@ -7,10 +7,7 @@ public class FamiliarController : MonoBehaviour
 {
     [SerializeField] private List<Transform> familiarList;
 
-    [SerializeField] private int familiarCount;
-    [SerializeField] private float distanceFromCenter;
-    [SerializeField] private float rotationSpeed;
-    private float maxY = 0.5f, minY = -0.5f;
+    private float maxY = 0.5f;
 
     private void Start()
     {
@@ -22,27 +19,24 @@ public class FamiliarController : MonoBehaviour
         foreach (Transform familiar in familiarList)
         {
             //familiar yukarý aþaðý hareketi
-            float familiarMoveSpeed = UnityEngine.Random.Range(2f, 5f);
-            familiar.transform.DOLocalMoveY(maxY, familiarMoveSpeed)
+            float familiarCycleDuration = UnityEngine.Random.Range(2f, 5f);
+            familiar.transform.DOLocalMoveY(maxY, familiarCycleDuration)
                     .SetEase(Ease.InOutSine)
                     .SetLoops(-1, LoopType.Yoyo)
                     ;
-            //eliptik yörüngede dönmesi
-            familiar.transform.DORotate(new Vector3(
-                familiar.transform.rotation.x, 25f, familiar.transform.rotation.z), 3)
+            //kendi etrafýnda dönmesi
+            familiar.transform.DORotate(
+                new Vector3(familiar.transform.rotation.x, 25f, familiar.transform.rotation.z),
+                familiarCycleDuration * 0.5f)
+                .SetEase(Ease.Linear)
                 .SetLoops(-1, LoopType.Yoyo)
                 ;
-
-
+            //eliptik yörüngede dönmesi
+            transform.DORotate(new Vector3(transform.rotation.x, 360, transform.rotation.z),
+                10f, RotateMode.FastBeyond360)
+                .SetEase(Ease.InOutSine)
+                .SetLoops(-1, LoopType.Restart)
+                ;
         }
-
     }
-
-    private void Update()
-    {
-
-    }
-
-
-
 }
