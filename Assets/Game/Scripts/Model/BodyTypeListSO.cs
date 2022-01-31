969b2dfe5bbe0776ff60d7971e287b6986bbf7ef
OmerAsik <faruk.asikoglu@gmail.com>
2022-01-31 12:44:05 +0300

    Polish

--- Assets/Game/Scripts/Model/BodyTypeListSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptable Objects/List/BodyList")]
-public class BodyTypeListSO : ScriptableObject
-{
-    public List<BodySO> list;
-}
--- Assets/Game/Scripts/Model/DressTypeListSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "Scriptable Objects/List/DressList")]
-public class DressTypeListSO : ScriptableObject
-{
-    public List<DressSO> list;
-}
